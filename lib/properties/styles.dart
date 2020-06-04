851b8badff0c85d96bc3d6193b13764de05bd44c
Hannah Duah <handuah@gmail.com>
2020-06-04 11:57:41 +0100

    added some new styles to file

--- lib/properties/styles.dart
+++ lib/properties/styles.dart
@@ -6,5 +6,26 @@ abstract class Styles {
                                                       fontSize: 12,
                                                       fontStyle: FontStyle.normal,
                                                       color: Colors.white,
+                                                      fontWeight: FontWeight.w600,
                                                     );
+  static const TextStyle storyUsername = TextStyle(
+    fontSize: 11,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+
+   static const TextStyle storyComments = TextStyle(
+     fontSize: 11,
+    fontStyle: FontStyle.normal,
+    color: Colors.white54,
+    fontWeight: FontWeight.normal,
+  );
+   
+    static const TextStyle storyCaption = TextStyle(
+     fontSize: 11,
+    fontStyle: FontStyle.normal,
+    color: Colors.white,
+    fontWeight: FontWeight.normal,
+  );                                                
 }
